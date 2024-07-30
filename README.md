3b4a2b5315d0da6a240108d6b7ea7561a69702d3
gilhan <rlfgks97@gmail.com>
2024-07-31 01:54:13 +0900

    feat: update dev settings

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # what-was
+
+### cmd
+```sh
+npm run start # for start
+npm run dev # for develop
+```
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) =>
-  res.send("Hi Claire Welcome to Node.js")
+  res.send("Main url")
 );
 
 module.exports = router;
