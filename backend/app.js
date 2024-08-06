b9d70b5d5f22fb3369b05da53b6ea822cff7868f
gilhan <rlfgks97@gmail.com>
2024-08-06 23:45:19 +0900

    feat: update cors policy for backend server

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const createError = require('http-errors');
 const express = require('express');
+const cors = require('cors');
 // var path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
@@ -11,6 +12,16 @@ const openaiRouter = require('./routes/searchRouter');
 const feedbackRouter = require('./routes/feedback');
 
 const app = express();
+const corsOptions = {
+  origin: '*',
+  // method: [
+  //   'GET',
+  //   'POST',
+  // ],
+  // allowedHeaders: [
+  //   'Content-Type',
+  // ]
+}
 
 app.set('pool', pool);
 
@@ -24,6 +35,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors(corsOptions));
 
 app.use('/api', indexRouter);
 app.use('/api/users', usersRouter);
